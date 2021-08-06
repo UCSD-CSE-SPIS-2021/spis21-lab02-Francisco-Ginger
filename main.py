5c0e3bd594a4d682ff6015d2c0e449e6d09f5806
Francisco Garcia <88344324+FrancGarcia@users.noreply.github.com>
2021-08-06 20:44:22 +0000

    We added two more test cases as well as the first three.

--- main.py
+++ main.py
@@ -54,6 +54,22 @@ x = 236
 print(sumDigits(x))
 
 
+# Step 4
+def convertWageMtoW(mWage):
+
+   wageGap = 0.182
+
+   ratio = 1 - wageGap
+
+   return mWage * ratio
 
 
+print(convertWageMtoW(100))
 
+print(convertWageMtoW(76.2))
+
+print(convertWageMtoW(0))
+
+print(convertWageMtoW(50))
+
+print(convertWageMtoW(25.55))
